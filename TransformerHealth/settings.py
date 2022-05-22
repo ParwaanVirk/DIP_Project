c641a59cd496999a3992029fb1ef9396404836ee
ParwaanVirk <parwaanvirk@gmail.com>
2022-05-22 23:56:38 +0530

    removed heroku dependencies

--- TransformerHealth/settings.py
+++ TransformerHealth/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,8 +44,6 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     'login',
-    # "admin_honeypot",
-
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -147,4 +144,3 @@ ADMIN_HONEYPOT_EMAIL_ADMINS = False
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
