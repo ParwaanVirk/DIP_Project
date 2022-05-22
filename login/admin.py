dbf37d91eb4e1e46c4c150e1201fc442b8c6b562
ParwaanVirk <parwaanvirk@gmail.com>
2022-05-23 01:27:09 +0530

    added admin to user to hide password

--- login/admin.py
+++ login/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from login.models import *
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
 
 
-admin.site.register(Account)
+class AcconutAdmin(UserAdmin):
+    list_display = ('email', 'username', 'last_login', 'is_admin')
+    readonly_fields = ('date_joined', 'last_login')
+    search_fields = ()
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AcconutAdmin)
