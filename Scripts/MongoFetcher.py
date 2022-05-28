0349ea3e29274e4091b33a4dda1b04c5c499ce5b
ParwaanVirk <parwaanvirk@gmail.com>
2022-05-28 13:54:21 +0530

    mongo fetcher integration

--- Scripts/MongoFetcher.py
+++ Scripts/MongoFetcher.py
@@ -1,3 +1,5 @@
+from psycopg2 import Timestamp
+from backend.models import Transformer, TransData
 def get_database():
     from pymongo import MongoClient
     import pymongo
@@ -9,14 +11,52 @@ def get_database():
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
 
     client = MongoClient(CONNECTION_STRING)
-    db = client.admin
+    db = client['myFirstDatabase']
+
+    col = db['transformerdatas']
+
+    x = col.find()
+    
+    # print(x[0]) -- prints one transformer param instance
+    for data in x:
+        trans_id = data['transformerId']
+        current_trans = Transformer.objects.filter(id = trans_id)
+
+        TransData.objects.create(
+            transformer = current_trans,
+            Timestamp = data['timeStamp'],
+            Current_Input = data['currentInput'],
+            Voltage_Input = data['voltageInput'],
+            Oil_Temprature = data['oilTemperature'],
+            Winding_Temprature = 0,
+            Oil_TankLevel = data['oilTankLevel'], 
+            Moisture_Level = data['moistureLevel'],
+            Tapping_Ratio = data['tappingRatio'],
+            Overall_Health = data['overallHealth'],
+
+        )
+        
+
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
 
     
-    return db.command("serverStatus")
     
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":    
-    
     # Get the database
-    dbname = get_database()
-    print(dbname)
+    get_database()
